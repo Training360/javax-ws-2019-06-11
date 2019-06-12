d46c5da55321268cbffe054fc96a85b7baacd058
vicziani <viczian.istvan@gmail.com>
2019-06-12 11:19:15 +0200

    XPAth

---
+++ xmlsamples/src/main/java/xpath/BookPath.java
@@ -0,0 +1,25 @@
+package xpath;
+
+import org.w3c.dom.Element;
+import org.xml.sax.InputSource;
+
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
+import java.io.InputStream;
+
+public class BookPath {
+
+    public void getTitle(int i, InputStream inputStream) {
+        var p = "/catalog/book[" + i + "]/title";
+        var factory = XPathFactory.newInstance();
+        var xpath = factory.newXPath();
+        try {
+            var expr = xpath.compile(p);
+            var title = (Element) expr.evaluate(new InputSource(inputStream), XPathConstants.NODE);
+            System.out.println(title.getTextContent());
+        }
+        catch (Exception e) {
+            throw new RuntimeException("Error", e);
+        }
+    }
+}
