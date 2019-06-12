cf44677260bfb55523e9b362fe090263d8d9c2d2
vicziani <viczian.istvan@gmail.com>
2019-06-12 13:50:03 +0200

    JAXB

--- jaxbsample/src/main/java/jaxb/BookXmlWriter.java
+++ jaxbsample/src/main/java/jaxb/BookXmlWriter.java
@@ -1,10 +1,27 @@
 package jaxb;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 
 public class BookXmlWriter {
 
     public void writeXml(Book book, OutputStream outputStream) {
-        var context = new JAXBContext
+        try {
+            var context = JAXBContext.newInstance(Book.class);
+            var marshaller = context.createMarshaller();
+            marshaller.marshal(book, outputStream);
+        }
+        catch (JAXBException e) {
+            throw new RuntimeException("Error", e);
+        }
+    }
+
+    public static void main(String[] args) {
+        var book = new Book("Joseph Heller", "22-es csapdája", 1970, "XY12345");
+        var outputStream = new ByteArrayOutputStream();
+        new BookXmlWriter().writeXml(book, outputStream);
+        System.out.println(new String(outputStream.toByteArray()));
     }
 }
