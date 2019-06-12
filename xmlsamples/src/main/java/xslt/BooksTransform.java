84b7a7e8e2271fd15dce2f9a95be1727b3e61ce3
vicziani <viczian.istvan@gmail.com>
2019-06-12 13:19:33 +0200

    main

--- xmlsamples/src/main/java/xslt/BooksTransform.java
+++ xmlsamples/src/main/java/xslt/BooksTransform.java
@@ -3,6 +3,7 @@ package xslt;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 
@@ -20,5 +21,13 @@ public class BooksTransform {
         catch (Exception e) {
             throw new RuntimeException("Error", e);
         }
+    }
+
+    public static void main(String[] args) {
+        var outputStream = new ByteArrayOutputStream();
+        new BooksTransform().transformToHtml(BooksTransform.class
+                .getResourceAsStream("/books.xml"),
+                outputStream);
+        System.out.println(new String(outputStream.toByteArray()));
     }
 }
