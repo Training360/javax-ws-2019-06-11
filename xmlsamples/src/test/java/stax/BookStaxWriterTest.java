3f21a8cafb13fd13650d9e6d77a6a7af89d1b937
vicziani <viczian.istvan@gmail.com>
2019-06-12 11:50:41 +0200

    xmlunit

--- xmlsamples/src/test/java/stax/BookStaxWriterTest.java
+++ xmlsamples/src/test/java/stax/BookStaxWriterTest.java
@@ -5,6 +5,10 @@ import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.util.List;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+import static org.xmlunit.matchers.EvaluateXPathMatcher.hasXPath;
+
 public class BookStaxWriterTest {
 
     @Test
@@ -14,5 +18,8 @@ public class BookStaxWriterTest {
         new BookStaxWriter().writeXml(titles, baos);
         var xml = new String(baos.toByteArray());
         System.out.println(xml);
+
+        assertThat(xml, hasXPath("/catalog/book[3]/title",
+                equalTo("Java es XML")));
     }
 }
