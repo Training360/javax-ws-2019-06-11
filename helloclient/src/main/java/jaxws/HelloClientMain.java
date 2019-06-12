c000212744f61356af500ade078aff6fe4813276
vicziani <viczian.istvan@gmail.com>
2019-06-12 16:28:26 +0200

    different url

--- helloclient/src/main/java/jaxws/HelloClientMain.java
+++ helloclient/src/main/java/jaxws/HelloClientMain.java
@@ -1,13 +1,22 @@
 package jaxws;
 
+import com.learnwebservices.services.hello.HelloEndpoint;
 import com.learnwebservices.services.hello.HelloEndpointService;
 import com.learnwebservices.services.hello.HelloRequest;
+
+import javax.xml.namespace.QName;
+import javax.xml.ws.BindingProvider;
 
 public class HelloClientMain {
 
     public static void main(String[] args) {
         var service = new HelloEndpointService();
         var port = service.getHelloEndpointPort();
+
+        var bp = (BindingProvider)port;
+        bp.getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY,
+                "http://localhost:8088/hello");
+
 
         var request = new HelloRequest();
         request.setName("Oktato");
