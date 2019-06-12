487407e0bdebb685514e893a8abee184b4760a77
vicziani <viczian.istvan@gmail.com>
2019-06-12 16:08:14 +0200

    main

---
+++ helloclient/src/main/java/jaxws/HelloClientMain.java
@@ -0,0 +1,19 @@
+package jaxws;
+
+import com.learnwebservices.services.hello.HelloEndpointService;
+import com.learnwebservices.services.hello.HelloRequest;
+
+public class HelloClientMain {
+
+    public static void main(String[] args) {
+        var service = new HelloEndpointService();
+        var port = service.getHelloEndpointPort();
+
+        var request = new HelloRequest();
+        request.setName("Oktato");
+
+        var response = port.sayHello(request);
+
+        System.out.println(response.getMessage());
+    }
+}
