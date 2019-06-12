756020ca814dd555891e83a07a9c6b413761526e
vicziani <viczian.istvan@gmail.com>
2019-06-12 11:33:19 +0200

    Tesztelés XPath-szal

--- xmlsamples/src/test/java/stax/BookStaxWriterTest.java
+++ xmlsamples/src/test/java/stax/BookStaxWriterTest.java
@@ -1,4 +1,18 @@
 package stax;
 
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.util.List;
+
 public class BookStaxWriterTest {
+
+    @Test
+    public void testWriteXml() {
+        var titles = List.of("Micimacko", "22-es csapdaja", "Java es XML");
+        var baos = new ByteArrayOutputStream();
+        new BookStaxWriter().writeXml(titles, baos);
+        var xml = new String(baos.toByteArray());
+        System.out.println(xml);
+    }
 }
