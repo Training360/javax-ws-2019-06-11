de3b136c30e9c38889fe3222679089b1cc515e50
vicziani <viczian.istvan@gmail.com>
2019-06-12 10:29:30 +0200

    validacio

---
+++ xmlsamples/src/main/java/xsd/BookValidator.java
@@ -0,0 +1,24 @@
+package xsd;
+
+import javax.xml.XMLConstants;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.SchemaFactory;
+import java.io.InputStream;
+
+public class BookValidator {
+
+    public void validateXml(InputStream inputStream) {
+        var factory = SchemaFactory
+                .newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+        try {
+            var schema = factory.newSchema(
+                    new StreamSource(
+                            BookValidator.class.getResourceAsStream("/books.xsd")));
+            var validator = schema.newValidator();
+            validator.validate(new StreamSource(inputStream));
+        }
+        catch (Exception e) {
+            throw new RuntimeException("Error", e);
+        }
+    }
+}
