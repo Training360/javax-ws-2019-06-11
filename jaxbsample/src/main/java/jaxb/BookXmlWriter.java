c2e93577849926015299db6e21a36c576702e538
vicziani <viczian.istvan@gmail.com>
2019-06-12 13:43:02 +0200

    JAXB fuggoseg

---
+++ jaxbsample/src/main/java/jaxb/BookXmlWriter.java
@@ -0,0 +1,10 @@
+package jaxb;
+
+import java.io.OutputStream;
+
+public class BookXmlWriter {
+
+    public void writeXml(Book book, OutputStream outputStream) {
+        var context = new JAXBContext
+    }
+}
